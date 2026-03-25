dca50c64490750c50c92aa58ea264b66112a8414
TanSang <tansangnts2004@gmail.com>
2026-03-25 15:59:38 +0700

    featch: push code hoàn thành bài tập 1

---
+++ src/main/java/NguyenTanSang/NguyenTanSang/NguyenTanSangApplication.java
@@ -0,0 +1,13 @@
+package NguyenTanSang.NguyenTanSang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenTanSangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenTanSangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NguyenTanSang/NguyenTanSang/NguyenTanSangApplicationTests.java
@@ -0,0 +1,13 @@
+package NguyenTanSang.NguyenTanSang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenTanSangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
